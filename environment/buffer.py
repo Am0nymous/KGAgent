73694dc8a390f882421f03fa0be0a11f5ff83e4a
Am0nymous <105718288+Am0nymous@users.noreply.github.com>
2022-05-20 15:46:34 +0800

    Add files via upload

---
+++ environment/buffer.py
@@ -0,0 +1,113 @@
+﻿import torch
+import numpy as np
+import functools
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+GAMMA =0.5
+GAE_LAMBDA = 0.95
+def cat(list_of_tensors, dim=0):
+    return functools.reduce(lambda x, y: torch.cat([x, y], dim=dim), list_of_tensors)
+
+
+def catcat(list_of_lists_of_tensors, dim_outer=0, dim_inner=0):
+    return cat([cat(inner_list, dim_inner) for inner_list in list_of_lists_of_tensors], dim_outer)
+
+
+def discounted(vals, gamma=0.99):
+    G = 0
+    discounted = torch.zeros_like(vals)
+    for i in np.arange(vals.shape[-1]-1, -1, -1):
+        G = vals[..., i] + gamma * G
+        discounted[..., i] = G
+    return discounted
+
+
+def advantage(rewards, values):
+    if GAE_LAMBDA == 0:
+        returns = discounted(rewards, GAMMA)
+        advantage = returns - values
+    else:
+        values = torch.cat([values, torch.zeros((values.shape[0], 1, 1)).cuda()], dim=2)
+        deltas = rewards + GAMMA * values[..., 1:] - values[..., :-1]
+        advantage = discounted(deltas, GAMMA * GAE_LAMBDA)
+    return advantage
+
+
+class Buffer:
+    def __init__(self):
+        self.count = 0
+        self.trg_num = 0
+        self.first_final_reward = 0
+        self.first_rewards_vocab = 0
+        self.first_rewards_src = 0
+        self.first_value = 0
+        self.rewards = []
+        self.rewards_final = []
+        self.rewards_vocab = []
+        self.rewards_src = []
+        self.values = []
+        self.actions = []
+        self.action_logprobs = []
+        self.action_vocab_logits = []
+        self.action_src_logits = []
+
+    def __len__(self):
+        return self.count
+
+    def start_trajectory(self):
+        self.count += 1
+
+    def log_step(self, values, reward_final, reward_vocab, reward_src):
+        self.rewards_final.append(reward_final)
+        self.rewards_vocab.append(reward_vocab)
+        self.rewards_src.append(reward_src)
+        self.values.append(values)
+        if len(self.rewards_final) >= 2:
+            self.rewards_final[0] = self.first_final_reward * 0.999 + reward_final * 0.001
+            self.rewards_final.pop()
+            self.rewards_vocab[0] = self.first_rewards_vocab * 0.999 + reward_vocab * 0.001
+            self.rewards_vocab.pop()
+            self.rewards_src[0] = self.first_rewards_src * 0.999 + reward_vocab * 0.001
+            self.rewards_src.pop()
+            self.values[0] = self.first_value * 0.999 + values * 0.001
+            self.values.pop()
+
+
+    def get_returns_and_advantages(self, rewards, values):
+        returns = [discounted(rewards[:, i, :].unsqueeze(1), GAMMA).transpose(2, 1)
+                   for i in range(rewards.size(1))]
+        advantages = [advantage(rewards[:, i, :].unsqueeze(1), values[:, i, :].unsqueeze(1)).transpose(2, 1)
+                      for i in range(rewards.size(1))]
+        returns = cat([return_single.unsqueeze(0)for return_single in returns], dim=0).permute(3, 1, 0, 2)
+        advantages = cat([return_single.unsqueeze(0) for return_single in advantages], dim=0).permute(3, 1, 0, 2)
+
+        return returns, advantages
+
+    def get_samples(self):
+        samples = [self.rewards_final,
+                   self.rewards_vocab,
+                   self.rewards_src,
+                   self.values,
+                   self.actions,
+                   self.action_vocab_logits,
+                   self.action_src_logits,
+                   self.action_logprobs]
+        samples += self.get_returns_and_advantages(self.rewards_final[0], self.values[0])
+        samples += self.get_returns_and_advantages(self.rewards_vocab[0], self.values[0])
+        samples += self.get_returns_and_advantages(self.rewards_src[0], self.values[0])
+        return [cat(sample) for sample in samples], self.trg_num
+
+    def returns_and_advantages(self):
+        rewards_final = self.get_returns_and_advantages(self.rewards_final[0], self.values[0])
+        rewards_vocab = self.get_returns_and_advantages(self.rewards_vocab[0], self.values[0])
+        rewards_src = self.get_returns_and_advantages(self.rewards_src[0], self.values[0])
+        return cat(rewards_final), cat(rewards_vocab), cat(rewards_src),
+
+    def clear(self):
+        self.count = 0
+        self.trg_num = 0
+        self.rewards_final.clear()
+        self.rewards_vocab.clear()
+        self.rewards_src.clear()
+        self.values.clear()
+
---
+++ environment/environment.py
@@ -0,0 +1,86 @@
+﻿import torch
+import torch.nn as nn
+from torch.distributions import Categorical
+import functools
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def action_process(vocab_logits, copy_logits,
+                   tgt_s_num, tgt_s_len, b_size,
+                   nograd, action, deterministic):
+    action_logit = (vocab_logits, copy_logits)
+    if nograd == True:
+        action = action_from_logits(action_logit, deterministic=False)
+    else:
+        action = action
+    action_logprob, action_entropy = action_stats(action_logit, action)
+    action_logit = \
+        (action_logit[0].view(b_size, tgt_s_num, tgt_s_len, action_logit[0].size(-1)),
+         action_logit[1].view(b_size, tgt_s_num, tgt_s_len, action_logit[1].size(-1)))
+    action_logprob = \
+        action_logprob.view(b_size, tgt_s_num, tgt_s_len, action_logprob.size(-1))
+    return action, action_logit, action_logprob, action_entropy
+
+def action_from_logits(logits, deterministic=True):
+    distributions = _get_distributions(*logits)
+    actions = _get_actions(*(distributions + (deterministic,)))
+    return torch.stack(actions).transpose(1, 0)
+
+def action_stats(logits, action):
+    distributions = _get_distributions(*logits)
+    logprobs, entropies = _get_logprob_entropy(*(distributions + (action[:, 0], action[:, 1])))
+    return torch.stack(logprobs).transpose(1, 0), torch.stack(entropies).transpose(1, 0)
+
+def _get_distributions(action_logits_t, action_logits_r):
+    distribution_t = Categorical(logits=action_logits_t)
+    distribution_r = Categorical(logits=action_logits_r)
+    return distribution_t, distribution_r
+
+def _get_actions(distribution_t, distribution_r, deterministic=True):
+    if deterministic:
+        action_t = torch.argmax(distribution_t.probs, dim=-1)
+        action_r = torch.argmax(distribution_r.probs, dim=-1)
+    else:
+        action_t = distribution_t.sample()
+        action_r = distribution_r.sample()
+    return action_t, action_r
+
+def _get_logprob_entropy(distribution_t, distribution_r, action_t, action_r):
+    logprob_t = distribution_t.log_prob(action_t)
+    logprob_r = distribution_r.log_prob(action_r)
+    entropy_t = distribution_t.entropy()
+    entropy_r = distribution_r.entropy()
+    return [logprob_t, logprob_r], [entropy_t, entropy_r]
+
+def cat(list_of_tensors, dim=0):
+    return functools.reduce(lambda x, y: torch.cat([x, y], dim=dim), list_of_tensors)
+
+
+
+def step(finished_word_state, input_feed_w):
+    finished_word_state_list = \
+        [finished_word_state[tmp_idx] for tmp_idx in range(len(finished_word_state))]
+    saved_input_feed_w = torch.stack(finished_word_state_list, dim=0)
+
+    input_feed_w += [saved_input_feed_w]
+    return input_feed_w
+
+def relative_reward(reward_1, reward_2):
+    better = (reward_1 > reward_2).float() * 0.5
+    same = (reward_1 == reward_2).float() * 0.1
+    worse = (reward_1 < reward_2).float() * 0.6
+
+    reward = better - worse - same
+    return reward
+
+def reward_distance(outputs, gt_embeddings, prev_dist=None):
+    dist = torch.cosine_similarity(outputs.view(gt_embeddings.size(0), gt_embeddings.size(1), -1, gt_embeddings.size(-1)),
+                                gt_embeddings, dim=-1).permute(2,0,1)
+    if prev_dist is not None:
+        better = (dist < prev_dist).float() * 0.5
+        same = (dist < prev_dist).float() * 0.1
+        worse = (dist < prev_dist).float() * 0.6
+
+        reward = better - worse - same
+        return reward, dist
+    else:
+        return torch.zeros_like(dist), dist
